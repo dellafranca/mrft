a63fca77e7b4cb0c0aec9447eafa329794847c72
dellafranca <dellafranca@hotmail.com>
2013-12-07 05:19:53 +0000

    bug fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express(),
+    http = require('http'),
     server = http.createServer(app),
     io = require('socket.io').listen(server),
     twitter = require('ntwitter'),
